0fb561fb7f08d0433660b5b6eac9b8b2f472c628
guilherme29 <gui.amado29@gmail.com>
2020-06-12 17:40:54 +0100

    updated test generation script

--- server/generate_tests.sh
+++ server/generate_tests.sh
@@ -1,1 +1,8 @@
-node arduino.js > test_results.txt
+cd ..
+mkdir tests_results
+
+# getting temperature and humidity
+node sensors/raspberry.js > tests_results/respberry_tests
+# getting moisture and light
+node sensors/arduino.js > tests_results/arduino_tests
+
