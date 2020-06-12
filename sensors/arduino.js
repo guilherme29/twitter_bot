377a5ac438a5a90d57bc15dc0c26a0c4247f53aa
guilherme29 <gui.amado29@gmail.com>
2020-06-12 17:49:01 +0100

    Deleted useless comments and added console.log() to the correct file (had been added to raspberry.js instead).

--- sensors/arduino.js
+++ sensors/arduino.js
@@ -1,15 +1,14 @@
 var five = require('johnny-five');
 var m = require('./sensors/moisture_sensor.js');
 var l = require('./sensors/light_sensor.js');
-var dht = require('./sensors/dht11_rasp.js');
 
 var board = new five.Board({
 	port: '/dev/ttyUSB0'
 });
 
 
-
 board.on("ready", function(){
+	console.log();
 	m.getMoisture(function(value){console.log(value)});
 	l.getLight(function(value){console.log(value)});
 
@@ -17,6 +16,3 @@ board.on("ready", function(){
   		process.exit(1);
   	}, 500);
 });
-
-//dht.getTemp(function(value){console.log(value)});
-//dht.getHumidity(function(value){console.log(value)})
--- sensors/raspberry.js
+++ sensors/raspberry.js
@@ -1,29 +1,9 @@
 var sensor = require("node-dht-sensor");
 
-/*
-function getTemp(callback){
-	sensor.read(11,4,function(err, temperature, humidity){
-		temp = temperature;
-		setTimeout(function(){callback(temp);}, 100);
-	});
-
-}
-
-function getHumidity(callback){
-	sensor.read(11,4,function(err, temperature, humidity){
-		humd = humidity;
-		setTimeout(function(){callback(humd);}, 100);
-	});
-
-}
-module.exports = {getTemp, getHumidity}
-*/
-
 sensor.read(11,4,function(err,temperature,humidity){
 	if(err){
 	   throw err;
 	}
-	console.log();
 	console.log(temperature);
 	console.log(humidity);
 
