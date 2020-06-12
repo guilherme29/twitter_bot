4b97a0cb6a50adcedded1bb0a2984a77d2ce8025
guilherme29 <gui.amado29@gmail.com>
2020-06-12 17:42:25 +0100

    Corrected typos.

--- server/generate_tests.sh
+++ server/generate_tests.sh
@@ -1,8 +1,8 @@
 cd ..
-mkdir tests_results
+mkdir test_results
 
 # getting temperature and humidity
-node sensors/raspberry.js > tests_results/respberry_tests
+node sensors/raspberry.js > tests_results/raspberry_tests
 # getting moisture and light
 node sensors/arduino.js > tests_results/arduino_tests
 
